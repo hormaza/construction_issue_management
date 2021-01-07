228fc25c4fec6c166c384bf982979ec4d5fc194f
Matias Lionel Hormaza Becerra <matias.hormaza@globant.com>
2021-01-07 14:04:07 -0300

    add navbar component

---
+++ src/features/counter/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import autodeskLogo from '../../assets/imgs/autodesk_logo.png';
+
+function Navbar() {
+    return (
+        <header>
+          <img src={autodeskLogo}></img>
+          <h1>Construction Issues Management</h1>
+        </header>
+    )
+}
+
+export default Navbar;
