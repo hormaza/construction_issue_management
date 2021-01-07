4a3381976afdd8118f2745177f419ab2798c8dc0
Matias Lionel Hormaza Becerra <matias.hormaza@globant.com>
2021-01-07 14:17:14 -0300

    fix identation on navbar

--- src/features/counter/Navbar.js
+++ src/features/counter/Navbar.js
@@ -2,12 +2,12 @@ import React from 'react';
 import autodeskLogo from '../../assets/imgs/autodesk_logo.png';
 
 function Navbar() {
-    return (
-        <header>
-          <img src={autodeskLogo}></img>
-          <h1>Construction Issues Management</h1>
-        </header>
-    )
+  return (
+    <header>
+      <img src={autodeskLogo}></img>
+      <h1>Construction Issues Management</h1>
+    </header>
+  )
 }
 
 export default Navbar;
