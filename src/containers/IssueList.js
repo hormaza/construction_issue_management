692a1273943b6a6a98e8a1aff576ab72b59e28cc
Matias Lionel Hormaza Becerra <matias.hormaza@globant.com>
2021-01-08 21:47:41 -0300

    starred issues logic and styles for issuelist component

--- src/containers/IssueList.js
+++ src/containers/IssueList.js
@@ -1,41 +1,65 @@
 import React from 'react';
 import styled from 'styled-components'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 function IssueList() {
   const issueList = useSelector(state => state.issueList);
+  const dispatch = useDispatch();
+
   const issueSortedList = issueList.sort(
     (a, b) => new Date(a.dueDate) - new Date(b.dueDate)
   )
 
+  const starredIssue = (id) => {
+    const starred = issueList.find(issue => issue.uniqueID === id)
+    dispatch({
+      type: 'STARREDISSUE',
+      payload: {
+        uniqueID: starred.uniqueID
+      }
+    });
+  }
+
   const issueHasPassed = (dueDate) => new Date(dueDate) - new Date() < 0;
   return (
     <Styled>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Creation Date</th>
-            <th>Due Date</th>
-            <th>Type</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            issueSortedList.map(issue => (
-              <tr className={issueHasPassed(issue.dueDate) ? "issuePassed" : ""}
-                key={issue.uniqueID}>
-                <td>{issue.uniqueID}</td>
-                <td>{issue.creationDate}</td>
-                <td>{issue.dueDate}</td>
-                <td>{issue.type}</td>
-                <td>{issue.description}</td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+      <div className="content">
+        <div className="table-header">
+          <h2>Issues Table</h2>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Description</th>
+              <th>Type</th>
+              <th>Due Date</th>
+              <th>Creation Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              issueSortedList.map(issue => (
+                <tr className={
+                  [
+                    issueHasPassed(issue.dueDate) ? "issuePassed" : "",
+                    issue.starred ? "starred" : ""
+                  ].join(" ")
+                }
+                  key={issue.uniqueID}
+                  onClick={() => starredIssue(issue.uniqueID)}
+                >
+                  <td>{issue.uniqueID}</td>
+                  <td>{issue.description}</td>
+                  <td>{issue.type}</td>
+                  <td>{issue.dueDate}</td>
+                  <td>{issue.creationDate}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     </Styled>
   )
 }
@@ -43,10 +67,47 @@ function IssueList() {
 export default IssueList;
 
 const Styled = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  justify-content: center;
+  margin: 20px 0;
+  .content {
+    width: 100%;
+    max-width: 1000px;
+    box-shadow: 0 10px 16px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+    border-radius: 10px;
+    overflow: hidden;
+  }
+
+  .table-header{
+    width: 100%;
+    background-color: #eff0f1;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    h2 {
+      font-size: 30px;
+      text-align: center;
+      color: #666;
+      padding: 20px 0;
+      margin: 0;
+      border-bottom: 1px solid #cccccc;
+    }
+  }
+  
   table {
     font-family: arial, sans-serif;
     border-collapse: collapse;
     width: 100%;
+  }
+
+  tbody{
+    tr {
+      &:hover {
+        background: #1bba939e;
+        cursor: pointer;
+      }
+    }
   }
 
   td, th {
@@ -56,6 +117,12 @@ const Styled = styled.section`
   }
 
   .issuePassed {
-    background: red;
+    color: orangered !important;
+  }
+
+  .starred {
+    background-color: #1bba93;
+    font-weight: bold;
+    color: white;
   }
 `
